f48f2597b899b86bc5e2c4c83452f42f7121ed2e
Mani Monajjemi <mani.monajjemi@gmail.com>
2014-08-11 12:16:25 -0700

    Benchmarker moved to its own hpp/cpp. First loop closure for camera tracking. No debuging yet

---
+++ include/obzerver/benchmarker.hpp
@@ -0,0 +1,28 @@
+#ifndef BENCHMARKER_H
+#define BENCHMARKER_H
+
+#include <vector>
+
+/* Singleton */
+class StepBenchmarker {
+private:
+    long int last_tick;
+    typedef std::pair<std::string, double> tick_t;
+    std::vector<tick_t> items;
+    double update();
+
+    StepBenchmarker();
+    // Not implementing these two prevents changing the instance
+    StepBenchmarker(const StepBenchmarker& rhs);
+    const StepBenchmarker& operator=(const StepBenchmarker& rhs);
+public:
+    static StepBenchmarker& GetInstance();
+    void reset();
+    void tick();
+    void tick(const std::string& text);
+    std::string getstr(const bool clear_screen = false) const;
+    void dump(const bool clear_screen = false) const;
+};
+
+
+#endif
---
+++ src/benchmarker.cpp
@@ -0,0 +1,61 @@
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include "obzerver/benchmarker.hpp"
+#include "opencv2/core/core.hpp"
+
+StepBenchmarker::StepBenchmarker(): last_tick(cv::getTickCount()) {
+    reset();
+}
+
+StepBenchmarker& StepBenchmarker::GetInstance() {
+  static StepBenchmarker instance;
+  return instance;
+}
+
+void StepBenchmarker::reset() {
+    items.clear();
+    items.push_back(std::make_pair("Start", 0.0));
+}
+
+void StepBenchmarker::tick() {
+    std::stringstream stream;
+    stream << "Unknown " << items.size();
+    items.push_back(std::make_pair(stream.str(), update()));
+}
+
+void StepBenchmarker::tick(const std::string& text) {
+    items.push_back(std::make_pair(text, update()));
+}
+
+std::string StepBenchmarker::getstr(const bool clear_screen) const {
+  std::stringstream stream;
+  double total = 0;
+  for (unsigned int i = 0; i < items.size(); i++) {
+    stream << std::setw(30) << std::left
+           << items.at(i).first
+           << std::setw(10)
+           << items.at(i).second << " (ms)"
+           << std::endl;
+    total += items.at(i).second;
+  }
+  stream << std::setw(30) << std::left
+         << "[Total]" << std::setw(10) << total << " (ms)" << std::endl;
+  if (clear_screen) {
+    // From: http://stackoverflow.com/q/4062045/1215297
+    stream << "\033[2J\033[1;1H";
+  }
+  return stream.str();
+}
+
+void StepBenchmarker::dump(const bool clear_screen) const {
+  std::cerr << getstr(clear_screen);
+}
+
+double StepBenchmarker::update() {
+    long int now = cv::getTickCount();
+    const double diff = 1000.0 * ((double) (now - last_tick) / (double) cv::getTickFrequency());
+    last_tick = now;
+    return diff;
+}
