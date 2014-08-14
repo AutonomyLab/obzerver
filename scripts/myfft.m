f7c657a852cb01408788f55e54d7e94b590655d7
Mani Monajjemi <mani.monajjemi@gmail.com>
2014-08-14 16:12:52 -0700

    Added myfft octave script

---
+++ scripts/myfft.m
@@ -0,0 +1,33 @@
+function result = myfft(signal, Fs)
+	dt = 1/Fs;   
+	N = size(signal, 2);
+	StopTime = N * dt;
+	t = (0:dt:StopTime-dt);
+	dF = Fs/N;                      % hertz
+	f = 0:dF:Fs-dF;
+
+
+	hann_window = transpose(hanning(N));
+	avg = mean(signal);
+	
+	str = sprintf('Sampling Freq: %f Number of Samples: %d StopTime: %fi Avg: %f dF: %f', Fs, N, StopTime, avg, dF);
+	
+	disp(str);
+	%signal_filtered = (signal - avg);% .* hann_window;
+	signal_filtered = (signal - avg) .* hann_window;
+    signal_filtered = filter(ones(1,2)/2, 1, signal_filtered);
+	%signal_filtered = detrend(signal) .* hann_window;
+
+	ft_signal = fft(signal_filtered);
+	power_ft_signal = abs(ft_signal) / N;
+	
+	figure;
+	plot(t, signal); title('original'); grid on;
+	figure;
+	plot(t, signal_filtered); title('filtered'); grid on;
+	figure;
+	plot(t, detrend(signal)); title('detrend'); grid on;
+	figure;
+	plot(f(1:N/2), power_ft_signal(1:N/2)); title('|ft|'); grid on;
+
+	result = power_ft_signal;
