feb6a18ed5684fcff33ca59fdd7c2c6889f69fe0
Mani Monajjemi <mani.monajjemi@gmail.com>
2015-02-03 11:45:12 -0800

    Fix bug regarding ccv linking in CMake. Add apt-get installs to perf-test.sh

--- scripts/perf-test.sh
+++ scripts/perf-test.sh
@@ -9,6 +9,12 @@ echo "Temp Dir: $TMP_DIR"
 
 cd $TMP_DIR
 
+echo "Installing dependencies ..."
+
+sudo apt-get update -qq
+# Except for glog, all other are ccv deps (not all required though)
+sudo apt-get install -qy libgoogle-glog-dev clang libjpeg-dev libpng-dev libdispatch-dev libgsl0-dev liblas-dev libfftw3-dev liblinear-dev libavcodec-dev libavformat-dev libswscale-dev
+
 echo "Downloading samples ..."
 wget --quiet -nc http://autolab.cmpt.sfu.ca/files/waving_sample_1.tar.gz
 tar xfz waving_sample_1.tar.gz
@@ -32,5 +38,3 @@ do
   echo "Processing $file ..."
   ./obzerver/build/demo --fps=29.97 -v $file -l ./log/$file.log 1>&2 2>./result/$file.txt
 done
-
-
