5c1b179a3e419a0791867d6aed6a673de7089419
Mani Monajjemi <mani.monajjemi@gmail.com>
2015-02-22 15:12:43 -0800

    Minor cmake/readme modifications

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 ## Pre-requirements
 
+- Optimized OpenCV
 - Google glog: libgoogle-glog-dev
 - GNU Scientific Library: libgsl0-dev
+- git (for cmake external project)
+
+## Compile Instructions
+
+```bash
+mkdir build && cd build
+cmake .. -DCMAKE_PREFIX_PATH=/PATH/TO/OPENCV/INSTALL -DCMAKE_BUILD_TYPE=RelWithDebInfo
+```
--- scripts/perf-test.sh
+++ scripts/perf-test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-OPENCV_DIR=/home/autolab/Dev/opencv/opencv-2.4.10.1/build
+OPENCV_DIR=/local_home/opencv/opencv-2.4.10.1/install
+#OPENCV_DIR=/home/autolab/Dev/opencv/opencv-2.4.10.1/install
+
 TMP_DIR=`mktemp -d`
 
 echo "OpenCV Dir: $OPENCV_DIR"
@@ -20,13 +22,13 @@ wget --quiet -nc http://autolab.cmpt.sfu.ca/files/waving_sample_1.tar.gz
 tar xfz waving_sample_1.tar.gz
 
 echo "Cloning obzerver ..."
-git clone --quiet --recursive https://bitbucket.org/mani-monaj/obzerver.git
+git clone --quiet --recursive https://bitbucket.org/AutonomyLab/obzerver.git
 cd obzerver
 mkdir build
 cd build
 
 echo "Building obzerver ..."
-cmake -DUSE_CUSTOM_OPENCV=1 -DOpenCV_DIR=$OPENCV_DIR -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_PREFIX_PATH=${OPENCV_DIR} -DCMAKE_BUILD_TYPE=Release ..
 make -j2
 
 cd ../..
@@ -36,7 +38,12 @@ mkdir result
 for file in *.avi
 do
   echo "Processing $file ..."
-  ./obzerver/build/demo --fps=29.97 -v $file -l ./log/$file.log 1>&2 2>/dev/null
+  ./obzerver/build/demo \
+    --fps=29.97 \
+    --video $file \
+    --logfile ./log/$file.log \
+    --config ../config/ucf_arg.ini \
+    1>&2 2>/dev/null
 done
 
 echo "Generating Results ..."
