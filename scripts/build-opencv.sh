77677db9ec30afb8f8364c245da0f8d34064692f
Mani Monajjemi <mani.monajjemi@gmail.com>
2015-11-25 18:22:09 -0800

    Enable contrib in build-opencv 3.0 (ht @jakebruce)

--- scripts/build-opencv.sh
+++ scripts/build-opencv.sh
@@ -4,14 +4,22 @@ set -e
 VERSION=3.0.0
 
 sudo apt-get update -q
+sudo apt-get upgrade -y
 sudo apt-get install -y build-essential libgtk2.0-dev pkg-config libjpeg-dev libtiff4-dev libjasper-dev libopenexr-dev cmake python-dev python-numpy python-tk libtbb-dev libeigen3-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev libv4l-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev
 
 wget -nc https://github.com/Itseez/opencv/archive/$VERSION.tar.gz
-tar xfz $VERSION.tar.gz
+# took out -nc because in combination with -O it causes a non-zero return value even on success
+wget https://github.com/Itseez/opencv_contrib/archive/$VERSION.tar.gz -O opencv_contrib-$VERSION.tar.gz
+
+tar xfzv $VERSION.tar.gz
+tar xfzv opencv_contrib-$VERSION.tar.gz
+
+CONTRIB_DIR=`pwd`/opencv_contrib-$VERSION
+
 cd opencv-$VERSION
 rm -rf build install
 mkdir build install
 export OPENCV_INSTALL_PREFIX=`pwd`/install
 cd build
-cmake -D CMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_PREFIX ENABLE_SSE41=ON -D ENABLE_SSE42=ON -D ENABLE_SSSE3=ON ENABLE_SSE=ON ENABLE_SSE2=ON ENABLE_SSE3=ON ENABLE_AVX=ON ENABLE_AVX2=ON -D CMAKE_BUILD_TYPE=Release -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=OFF -D WITH_OPENGL=OFF -D WITH_VTK=OFF -D BUILD_DOCS=OFF -D BUILD_TESTS=OFF -D BUILD_WITH_DEBUG_INFO=ON -D WITH_CUDA=OFF -D WITH_CUFFT=OFF -D WITH_EIGEN=ON -D WITH_OPENCL=OFF ..
+cmake -D CMAKE_INSTALL_PREFIX=$OPENCV_INSTALL_PREFIX ENABLE_SSE41=ON -D ENABLE_SSE42=ON -D ENABLE_SSSE3=ON ENABLE_SSE=ON ENABLE_SSE2=ON ENABLE_SSE3=ON ENABLE_AVX=ON ENABLE_AVX2=ON -D CMAKE_BUILD_TYPE=Release -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=OFF -D INSTALL_PYTHON_EXAMPLES=OFF -D BUILD_EXAMPLES=OFF -D WITH_QT=OFF -D WITH_OPENGL=OFF -D WITH_VTK=OFF -D BUILD_DOCS=OFF -D BUILD_TESTS=OFF -D BUILD_WITH_DEBUG_INFO=ON -D WITH_CUDA=OFF -D WITH_CUFFT=OFF -D WITH_EIGEN=ON -D WITH_OPENCL=OFF -DOPENCV_EXTRA_MODULES_PATH=$CONTRIB_DIR/modules ..
 make all install -j 8
